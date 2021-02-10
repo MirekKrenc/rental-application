daeadaa312e06c592616a6c541d5a8b63bce5991
Mirek Krenc <kremir@o2.pl>
2021-02-11 00:17:54 +0100

    Adding new hotel room - refactoring by using stream to map rooms definition to SpaceInRoom objects

--- src/main/java/pl/krencu/rentalapplication/domain/hotelroom/HotelRoomFactory.java
+++ src/main/java/pl/krencu/rentalapplication/domain/hotelroom/HotelRoomFactory.java
@@ -1,14 +1,22 @@
 package pl.krencu.rentalapplication.domain.hotelroom;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class HotelRoomFactory {
     public HotelRoom create(String hotelId, int number, String description, Map<String, Double> roomsDefinition) {
 
-        List<SpaceInRoom> spaceInRooms = new ArrayList<>();
-        roomsDefinition.forEach((name, size) -> spaceInRooms.add(new SpaceInRoom(name, new SquareMeter(size))));
+        List<SpaceInRoom> spaceInRooms = roomsDefinition
+                .entrySet()
+                .stream()
+                .map(entry -> {
+                    SquareMeter squareMeter = new SquareMeter(entry.getValue());
+                    return new SpaceInRoom(entry.getKey(), squareMeter);
+                })
+                .collect(Collectors.toList());
+
+
         return new HotelRoom(hotelId, number, description, spaceInRooms);
     }
 }
