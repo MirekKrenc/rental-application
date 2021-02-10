568f69f0e29bc704a10a7182fa73f24961b60af9
Mirek Krenc <kremir@o2.pl>
2021-02-10 14:02:52 +0100

    Adding new apartment - encapsulation when needed

--- src/main/java/pl/krencu/rentalapplication/domain/apartment/Room.java
+++ src/main/java/pl/krencu/rentalapplication/domain/apartment/Room.java
@@ -1,10 +1,10 @@
 package pl.krencu.rentalapplication.domain.apartment;
 
-public class Room {
+class Room {
     private final String name;
     private final SquareMeter squareMeter;
 
-    public Room(String name, SquareMeter squareMeter) {
+    Room(String name, SquareMeter squareMeter) {
         this.name = name;
         this.squareMeter = squareMeter;
     }
