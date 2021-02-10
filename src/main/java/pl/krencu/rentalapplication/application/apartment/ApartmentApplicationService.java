0cdb52c4759566647b6944d9e1c9a1af259ed97a
Mirek Krenc <kremir@o2.pl>
2021-02-10 13:58:35 +0100

    Adding new apartment - move creation of Apartment from ApartmentApplicationService to factory method (ApartmentFactory)

--- src/main/java/pl/krencu/rentalapplication/application/apartment/ApartmentApplicationService.java
+++ src/main/java/pl/krencu/rentalapplication/application/apartment/ApartmentApplicationService.java
@@ -1,12 +1,8 @@
 package pl.krencu.rentalapplication.application.apartment;
 
 import pl.krencu.rentalapplication.domain.apartment.Apartment;
-import pl.krencu.rentalapplication.domain.apartment.Room;
-import pl.krencu.rentalapplication.domain.apartment.Address;
-import pl.krencu.rentalapplication.domain.apartment.SquareMeter;
+import pl.krencu.rentalapplication.domain.apartment.ApartmentFactory;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class ApartmentApplicationService {
@@ -15,10 +11,7 @@ public class ApartmentApplicationService {
                     String apartmentNumber, String city, String country, String description,
                     Map<String, Double> roomsDefinition) {
 
-        Address address = new Address(street, postalCode, houseNumber, apartmentNumber, city, country);
-        List<Room> rooms = new ArrayList<>();
-        roomsDefinition.forEach((name, size) -> rooms.add(new Room(name, new SquareMeter(size))));
-
-        Apartment apartment = new Apartment(ownerId, address, description);
+        final Apartment apartment =  new ApartmentFactory().create(ownerId, street, postalCode, houseNumber, apartmentNumber, city, country, description, roomsDefinition);
     }
+
 }
